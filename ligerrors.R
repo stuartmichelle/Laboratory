e56c8196a97b1140a9c08af0917297af7e3a46ad
Michelle Stuart <bunx21@yahoo.com>
2016-08-02 23:14:21 -0400

    connect to database

--- ligerrors.R
+++ ligerrors.R
@@ -1,2 +1,6 @@
 # This is a script to find Ligations that need to be re-done due to lab error
 
+
+# Connect to database
+library(RMySQL)
+labor <- dbConnect(MySQL(), host="amphiprion.deenr.rutgers.edu", user="michelles", password="larvae168", dbname="Laboratory", port=3306)
