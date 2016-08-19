73227377fba347b4f1434ac118d23db1caeffb50
Michelle Stuart <michelle@vpn-client-172-16-8-155.rutgers.edu>
2016-08-19 11:52:23 -0400

    add lig list

--- ligerrors.R
+++ ligerrors.R
@@ -1,6 +1,12 @@
 # This is a script to find Ligations that need to be re-done due to lab error
 
 
-# Connect to database
-library(RMySQL)
-labor <- dbConnect(MySQL(), host="amphiprion.deenr.rutgers.edu", user="michelles", password="larvae168", dbname="Laboratory", port=3306)
+# Connect to databases
+suppressMessages(library(dplyr))
+labor <- src_mysql(dbname = "Laboratory", host = "amphiprion.deenr.rutgers.edu", user = "michelles", password = "larvae168", port = 3306, create = F)
+leyte <- src_mysql(dbname = "Leyte", host = "amphiprion.deenr.rutgers.edu", user = "michelles", password = "larvae168", port = 3306, create = F)
+
+# get the list of ligations that need to be regenotyped
+ligs <- leyte %>% tbl("known_issues") %>% select(Issue, contains("regenotype"))
+
+# make sure they haven't already been regenotyped and aren't on the list of good matches
