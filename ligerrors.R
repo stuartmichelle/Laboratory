179696a7cc1195920b19a4b8a4decb5545503455
Michelle Stuart <bunx21@yahoo.com>
2016-08-19 13:38:22 -0400

    make sure no lig error samples are on the good match list

--- ligerrors.R
+++ ligerrors.R
@@ -7,6 +7,20 @@ labor <- src_mysql(dbname = "Laboratory", host = "amphiprion.deenr.rutgers.edu",
 leyte <- src_mysql(dbname = "Leyte", host = "amphiprion.deenr.rutgers.edu", user = "michelles", password = "larvae168", port = 3306, create = F)
 
 # get the list of ligations that need to be regenotyped
-ligs <- leyte %>% tbl("known_issues") %>% select(Issue, contains("regenotype"))
+ligs <- data.frame(leyte %>% tbl("known_issues") %>% select(Ligation_ID, Issue, contains("regenotype")))
 
-# make sure they haven't already been regenotyped and aren't on the list of good matches
+# make sure they aren't on the list of good matches
+matches <- read.csv("~/Documents/Philippines/Genetics/2016-08-19matches.csv")
+
+bingo <- data.frame(Ligation_ID = character(0), Issue = character(0))
+
+for (i in 1:nrow(ligs)){
+bingo$Ligation_ID[i] <- ligs$Ligation_ID[which(ligs$Ligation_ID[i] == matches$First.ID)]
+}
+
+for (i in 1:nrow(ligs)){
+  bingo$Ligation_ID[i] <- ligs$Ligation_ID[which(ligs$Ligation_ID[i] == matches$Second.ID)]
+}
+
+# Find the extraction IDs of the samples to be regenotyped
+
