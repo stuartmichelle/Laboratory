7caa72b067e2d9638d38d9b31ee217f6c6302158
Michelle Stuart <bunx21@yahoo.com>
2016-11-01 14:21:21 -0400

    init

--- R/quantextr.R
+++ R/quantextr.R
@@ -7,24 +7,24 @@ extracol <- 2
 # Read digest platemap_list into R from database - find the samples in the plate and their well locations
 
 # which plate are you looking for?
-platename <- "E3247-E3339"
+platename <- "E2901-E2967"
 
 suppressMessages(library(dplyr))
 labor <- src_mysql(dbname = "Laboratory", host = "amphiprion.deenr.rutgers.edu", user = "michelles", password = "larvae168", port = 3306, create = F)
 
-diglist <- labor %>% tbl("digest") %>% filter(plate == platename) %>% select(digest_id, well) %>% collect()
+extrlist <- labor %>% tbl("extraction") %>% filter(plate == platename) %>% select(extraction_id, well) %>% collect()
 
 # separate out row from column in wells
-diglist$Row <- substr(diglist$well, 1, 1)
-diglist$Col <- substr(diglist$well, 2, 2)
+extrlist$Row <- substr(extrlist$well, 1, 1)
+extrlist$Col <- substr(extrlist$well, 2, 2)
 
 # split Col 1 into a second plate 
-dig2 <- diglist[(diglist$Col == 1), ]
-dig1 <- diglist[(diglist$Col != 1), ]
+extr2 <- extrlist[(extrlist$Col == 1), ]
+extr1 <- extrlist[(extrlist$Col != 1), ]
 
 
 # correct the locations of dig2 to account for being moved to second plate
-dig2$Col <- extracol
+extr2$Col <- extracol
 
 
 # Read in quantification results ------------------------------------------
@@ -45,7 +45,7 @@ dat1 <- dat1[1:(which(dat1$Sample == "Group Summaries")-1), ]
 # read in names for the samples
 quant1 <- dplyr::left_join(dat1, dig1, by = c("Wells" = "well"))
 quant1 <- quant1[ , c("ID", "AdjResult")]
-colnames(quant1) <- c("digest_id", "quant")
+colnames(quant1) <- c("extraction_id", "quant")
 
 # repeat for second plate
 platefile2 = "data/20160919_plate2.txt"
@@ -62,13 +62,13 @@ dat2 <- dat2[1:(which(dat2$Sample == "Group Summaries")-1), ]
 # read in names for the samples
 quant2 <- dplyr::left_join(dat2, dig2, by = c("Wells" = "well"))
 quant2 <- quant2[ , c("ID", "AdjResult")]
-colnames(quant2) <- c("digest_id", "quant")
+colnames(quant2) <- c("extraction_id", "quant")
 
 # join the 2 results lists
 quant <- rbind(quant1, quant2)
 
 # remove any empty wells
-quant <- quant[!is.na(quant$digest_id), ]
+quant <- quant[!is.na(quant$extraction_id), ]
 
 # Upload results to database ----------------------------------------------
 
@@ -77,23 +77,23 @@ quant <- quant[!is.na(quant$digest_id), ]
 # Retrieve the digest data from the database using dplyr
 
 # pull in all of the digest data
-suppressWarnings(digest <- labor %>% tbl("digest") %>% collect())
+suppressWarnings(extractiontbl <- labor %>% tbl("extraction") %>% collect())
 
 # add new quant data to existing data
 # for testing only quant$quant <- 8.008
 # which(digest_new$digest_id == "D3253")
 # digest_new$quant[3254] <- NA
 
-digest_new <- digest # make a copy of the original database table just in case something goes wrong
+extr_new <- extractiontbl # make a copy of the original database table just in case something goes wrong
 
-digest_new$quant <- ifelse(is.na(digest_new$quant), quant$quant[match(digest_new$digest_id, quant$digest_id)], digest_new$quant)
+extr_new$quant <- ifelse(is.na(extr_new$quant), quant$quant[match(extr_new$extraction_id, quant$extraction_id)], extr_new$quant)
 
 # append to the data in the database using RMySQL
 library(RMySQL)
 labors <- dbConnect(MySQL(), host="amphiprion.deenr.rutgers.edu", user="michelles", password="larvae168", dbname="Laboratory", port=3306)
 
 # Send data to database
-dbWriteTable(labors,"digest",data.frame(digest_new), row.names = FALSE, overwrite = TRUE)
+dbWriteTable(labors,"extraction",data.frame(extr_new), row.names = FALSE, overwrite = )
 
 dbDisconnect(labors)
 rm(labors)
