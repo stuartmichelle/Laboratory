a680a585d9e8c7ece32e72b9a66e755a59982f0f
Michelle Stuart <bunx21@yahoo.com>
2016-11-02 09:59:03 -0400

    adding in quant code

--- R/2_samples_have_been_extracted.R
+++ R/2_samples_have_been_extracted.R
@@ -1,45 +1,9 @@
-# This is a script for adding samples that are not in the database and have not been extracted yet
+# this script takes the samples from step 1 that have already been extracted 
+# and quantified and prepares them for import into the database
 
-# enter the interval of sample numbers to be extracted, species, and 2-digit year
-span <- 95:188
-species <- "TEST"
-year <- "16"
-
-# make a list of sample IDs in the order they are going to be extracted
-sampID <- paste(species, year, "_", formatC(span, width = 3, format = "d", flag = "0"), sep = "")
-
-# insert the negative controls
-sampID[95:96] <- "XXXX"
-
-# create a list of well numbers
-well <- 1:96
-
-# put the samples in order of extraction (with negative controls inserted)
-str1 <- cbind(well[1:11], sampID[1:11])
-str2 <- cbind(well[12], sampID[95]) # because the first blank is in the 12th position
-str3 <- cbind(well[13:60], sampID[12:59])
-str4 <- cbind(well[61], sampID[96]) # because the 2nd blank is in the 61st position
-str5 <- cbind(well[62:96], sampID[60:94])
-
-# and stick all of the rows together
-extr <- data.frame(rbind(str1, str2, str3, str4, str5))
-colnames(extr) <- c("well", "sample_id")
-
-# make a plate map of sample IDs (for knowing where to place fin clips)
-plate <- data.frame( Row = rep(LETTERS[1:8], 12), Col = unlist(lapply(1:12, rep, 8)))
-platelist <- cbind(plate, extr$sample_id)
-names(platelist) <- c("Row", "Col", "ID")
-# create a file to store the plate positions until after the extraction
-write.csv(platelist, file = paste(Sys.Date(), "extract_list.csv", sep = ""))
-platelist$ID <- as.character(platelist$ID)
-platemap <- as.matrix(reshape2::acast(platelist,platelist[,1] ~ platelist[,2]), value.var = platelist$ID)
-write.csv(platemap, file = paste(Sys.Date(), "extract_map.csv", sep = ""))
-# print this platemap and use to place fin clips in wells
-
-# clean up
-rm(str1, str2, str3, str4, str5, well, sampid, span, species, year)
-
-########################################################################
+# import data from step 1 (prep_samples_for_extraction.R)
+extrfile <- "data/2016-11-02_extract_list.csv"
+extr <- read.csv(extrfile, stringsAsFactors = F)
 
 # add the date of extraction
 extr$date <- as.Date("2016-09-06")
@@ -50,18 +14,11 @@ extr$method <- "DNeasy 96"
 # add the final volume after elution
 extr$final_vol <- 200
 
-names(extr) <- c("number", "sample_id", "date", "method", "final_vol")
-# fix extraction numbers so the order of extraction numbers matches the order of sample ids
-extr$number <- NULL
+# add temporary extraction number
 extr$number <- as.integer(1:96)
 
-
-
 # add well and plate data to extr table
-platelist$well <- paste(platelist$Row, platelist$Col, sep = "")
-platelist <- platelist[ , c("well", "id")]
-extr <- left_join(extr, platelist, by = c("sample_id" = "ID"), copy = T)
-
+extr$well <- paste(extr$Row, extr$Col, sep = "")
 
 ### ONLY DO THIS ONCE ### generate extract numbers for database
 
@@ -74,35 +31,101 @@ x <- n[1,]
 extr$number <- paste("E", (extr$number + x), sep = "")
 extr$plate <- paste(extr$number[1], "-", extr$number[nrow(extr)], sep = "")
 
-# run the quantextr.R to 
+
+# Create a plate map of quantification of extracts -------------------------
+
+# because one row of the quantification plate is taken up by standards so one column from 
+# the plate to be quantified has to be moved to a second plate (which could actually be 
+# a third or more plate)
+
+# WHICH COLUMN OF THE SECOND PLATE IS THE FIRST COLUMN IN?
+extracol <- 2
+
+# find the samples in the plate and their well locations
+
+# split Col 1 into a second plate 
+extr2 <- extr[(extr$Col == 1), ]
+extr1 <- extr[(extr$Col != 1), ]
+
+# correct the locations of the second list to account for being moved to second plate
+extr2$Col <- extracol
+
+# Read in quantification results ------------------------------------------
+
+# read in plate reader data for the first plate
+platefile1 = "data/20160919_plate1.txt"
+colsinplate1 = 2:12 # is this a full plate?
+
+strs <- readLines(platefile1, skipNul = T)
+linestoskip = (which(strs == "Group: Unk_Dilution"))
+
+dat1 <- read.table(text = strs,  skip = linestoskip, sep = "\t", fill = T, header = T, stringsAsFactors = F)
+
+# remove footer rows
+dat1 <- dat1[1:(which(dat1$Sample == "Group Summaries")-1), ]
+
+# read in names for the samples
+quant1 <- dplyr::left_join(dat1, dig1, by = c("Wells" = "well"))
+quant1 <- quant1[ , c("ID", "AdjResult")]
+colnames(quant1) <- c("extraction_id", "quant")
+
+# repeat for second plate
+platefile2 = "data/20160919_plate2.txt"
+colsinplate2 = 1 # or is this just the first column of the plate?
+
+strs <- readLines(platefile2, skipNul = T)
+linestoskip = (which(strs == "Group: Unk_Dilution"))
+
+dat2 <- read.table(text = strs,  skip = linestoskip, sep = "\t", fill = T, header = T, stringsAsFactors = F)
+
+# remove footer rows
+dat2 <- dat2[1:(which(dat2$Sample == "Group Summaries")-1), ]
+
+# read in names for the samples
+quant2 <- dplyr::left_join(dat2, dig2, by = c("Wells" = "well"))
+quant2 <- quant2[ , c("ID", "AdjResult")]
+colnames(quant2) <- c("extraction_id", "quant")
+
+# join the 2 results lists
+quant <- rbind(quant1, quant2)
+
+# remove any empty wells
+quant <- quant[!is.na(quant$extraction_id), ]
+
+# Upload results to database ----------------------------------------------
+
+### THIS STEP PULLS IN ALL OF THE DIGEST TABLE, ADDS THE QUANT DATA, AND THEN OVERWRITES ALL OF THE DIGEST TABLE WITH A NEW ONE CONTAINING THE NEW DATA, PROCEED WITH ***CAUTION*** ###
+
+# Retrieve the digest data from the database using dplyr
+
+# pull in all of the digest data
+suppressWarnings(extractiontbl <- labor %>% tbl("extraction") %>% collect())
+
+# add new quant data to existing data
+# for testing only quant$quant <- 8.008
+# which(digest_new$digest_id == "D3253")
+# digest_new$quant[3254] <- NA
+
+extr_new <- extractiontbl # make a copy of the original database table just in case something goes wrong
+
+extr_new$quant <- ifelse(is.na(extr_new$quant), quant$quant[match(extr_new$extraction_id, quant$extraction_id)], extr_new$quant)
+
+# append to the data in the database using RMySQL
+library(RMySQL)
+labors <- dbConnect(MySQL(), host="amphiprion.deenr.rutgers.edu", user="michelles", password="larvae168", dbname="Laboratory", port=3306)
+
+# Send data to database
+dbWriteTable(labors,"extraction",data.frame(extr_new), row.names = FALSE, overwrite = )
+
+dbDisconnect(labors)
+rm(labors)
 
 
 
-# # edit the plate reader document so that it can be imported - delete header/footer, open in excel, check columns and save as csv
-# # import plate reader results for quantificaiton
-# pr <- read.csv("data/20160908_plate3.csv", stringsAsFactors = F, header = T)
-# 
-# # # add extract numbers (this plate contains the results for E2968-2975 plus others not on this current plate)
-# # 
-# # # remove rows for non-plate results
-# # pr <- pr[1:8,]
-# # # add extract numbers
-# # pr$number <- 2968:2975
-# # pr$number <- paste("E", pr$number, sep="")
-# 
-# pr2 <- read.csv("data/20160908_plate2.csv", stringsAsFactors = F)
-# 
-# pr2$number <- 3072:3159
-# pr2$number <- paste("E", pr2$number, sep="")
-# names(pr2) <- c("Sample", "Wells", "Value", "R", "Result", "MeanResult", "SD", "CV", "Dilution", "quant","number")
-# 
-# 
-# extr1 <- merge(extr, pr, by.x = "number", by.y = "extraction_ID", all.x = T)
-# extr1 <- extr1[1:8,]
-# extr2 <- merge(extr, pr2[ , 10:11], by.x = "number", by.y = "number", all.x = T)
-# extr2 <- extr2[9:96,]
-# 
-# extr <- rbind(extr1,extr2)
+
+
+
+
 
 extr$dna <- extr$final_vol * extr$quant * 0.001
 
