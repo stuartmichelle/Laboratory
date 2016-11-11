ae26f2f3bf5b1d7f51af097815a5d2d87ee360ed
Michelle Stuart <bunx21@yahoo.com>
2016-11-11 16:06:08 -0500

    add seq and pool options

--- R/findsampleid.R
+++ R/findsampleid.R
@@ -4,8 +4,8 @@
 suppressMessages(library(dplyr))
 labor <- src_mysql(dbname = "Laboratory", host = "amphiprion.deenr.rutgers.edu", user = "michelles", password = "larvae168", port = 3306, create = F)
 
-# enter id of the labwork
-sample <- "D2813"
+# enter id of the labwork - "L2345", "SEQ17", "P037"
+sample <- "D2345"
 
 # Find the sample_ID
 if (substr(sample, 1,1) == "L"){
@@ -27,4 +27,24 @@ if (substr(sample, 1,1) == "D"){
 if (substr(sample, 1,1) == "E"){
   suppressWarnings(extr <- labor %>% tbl("extraction") %>% filter(extraction_id == sample) %>% select(extraction_id, sample_id) %>% collect())
 }
+
+if (substr(sample, 1,1) == "S"){
+  suppressWarnings(seq <- labor %>% tbl("pcr") %>% filter(SEQ == sample) %>% select(pcr_id, SEQ) %>% collect())
+  suppressWarnings(lig <- labor %>% tbl("ligation") %>% filter(pool %in% seq$pcr_id) %>% select(ligation_id, digest_id, pool) %>% collect())  # this line will return an error if there is only one pool in the seq, change %in% to ==
+  seq <- left_join(seq, lig, by = c("pcr_id" = "pool"))
+  suppressWarnings(dig <- labor %>% tbl("digest") %>% filter(digest_id %in% seq$digest_id) %>% select(extraction_id, digest_id) %>% collect)
+  seq <- left_join(seq, dig, by = "digest_id")
+  suppressWarnings(extr <- labor %>% tbl("extraction") %>% filter(extraction_id %in% seq$extraction_id) %>% select(extraction_id, sample_id) %>% collect())
+  seq <- left_join(seq, extr, by = "extraction_id")
+  rm(dig, extr, lig)
+}
+
+if (substr(sample, 1,1) == "P"){
+    suppressWarnings(lig <- labor %>% tbl("ligation") %>% filter(pool == sample) %>% select(pool, ligation_id, digest_id) %>% collect())
+    suppressWarnings(dig <- labor %>% tbl("digest") %>% filter(digest_id %in% lig$digest_id) %>% select(extraction_id, digest_id) %>% collect())
+    lig <- left_join(lig, dig, by = "digest_id")
+    suppressWarnings(extr <- labor %>% tbl("extraction") %>% filter(extraction_id %in% dig$extraction_id) %>% select(extraction_id, sample_id) %>% collect())
+    lig <- left_join(lig, extr, by = "extraction_id")
+    rm(dig, extr)
+  }
 
