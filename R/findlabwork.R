d1987f3a39b48a1928a6744222c4b7803678909e
Michelle Stuart <bunx21@yahoo.com>
2016-11-11 15:47:46 -0500

    fix errors when there are different codes required for 1 line results and multi line results

--- R/findlabwork.R
+++ R/findlabwork.R
@@ -5,7 +5,7 @@ suppressMessages(library(dplyr))
 labor <- src_mysql(dbname = "Laboratory", host = "amphiprion.deenr.rutgers.edu", user = "michelles", password = "larvae168", port = 3306, create = F)
 
 # enter sample you are looking for
-sample <- "APCL15_371766"
+sample <- "APCL13_230"
 
 # Find extractions
 work <- labor %>% tbl("extraction") %>% filter(sample_id == sample) %>% select(sample_id, extraction_id) %>% collect()
@@ -15,9 +15,18 @@ dig <- labor %>% tbl("digest") %>% filter(extraction_id == work$extraction_id) %
 work <- left_join(work, dig, by = "extraction_id")
 
 # Add ligations
-suppressWarnings(lig <- labor %>% tbl("ligation") %>% filter(digest_id %in% work$digest_id) %>% select(ligation_id, digest_id, pool) %>% collect())
+if (nrow(work) > 1){
+  suppressWarnings(lig <- labor %>% tbl("ligation") %>% filter(digest_id %in% work$digest_id) %>% select(ligation_id, digest_id, pool) %>% collect())
+}else{
+  suppressWarnings(lig <- labor %>% tbl("ligation") %>% filter(digest_id == work$digest_id) %>% select(ligation_id, digest_id, pool) %>% collect())
+}
 work <- left_join(work, lig, by = "digest_id")
+colnames(work) <- c("sample_id", "extraction_id", "digest_id", "ligation_id", "pcr_id")
 
 # Add SEQ
-suppressWarnings(seq <- labor %>% tbl("pcr") %>% filter(pcr_id %in% work$pool) %>% select(pcr_id, SEQ) %>% collect())
-work <- left_join(work, seq, by = c("pool" = "pcr_id"))
+if (nrow(work) > 1){
+  suppressWarnings(seq <- labor %>% tbl("pcr") %>% filter(pcr_id %in% work$pcr_id) %>% select(pcr_id, SEQ) %>% collect())
+}else{
+  suppressWarnings(seq <- labor %>% tbl("pcr") %>% filter(pcr_id == work$pcr_id) %>% select(pcr_id, SEQ) %>% collect())
+}
+work <- left_join(work, seq, by = "pcr_id")
