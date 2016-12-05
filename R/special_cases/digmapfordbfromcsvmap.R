755a897ca04a1814752d4cc78caa34e1078f2f6f
Michelle Stuart <bunx21@yahoo.com>
2016-12-05 14:54:25 -0500

    final working

--- R/special_cases/digmapfordbfromcsvmap.R
+++ R/special_cases/digmapfordbfromcsvmap.R
@@ -1,142 +1,86 @@
 # pull the csv plate maps into R and prep them to be uploaded to database
 
-final <- data.frame(Row = NA, Col = NA, Sample = NA, Plate = NA)
+# pull in the map
+sheet <- read.csv("data/D3247-D3339map.csv", as.is = T)
+  
+# rename the columns
+colnames(sheet) <- c("Row", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
 
+#remove any blank rows
+sheet <- sheet[!is.na(sheet$Row), ]
 
-  sheet <- read.csv("data/D3247-D3339map.csv", as.is = T)
-  colnames(sheet) <- c("Row", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
-  sheet <- sheet[!is.na(sheet$Row), ]
-  sheet <- sheet[1:8, ]
-  data <- reshape2::melt(sheet, id.vars = "Row", variable.name = "Col", value.name = "Sample")
-  data <- data[!is.na(data$Sample), ]
-  for (j in 1:nrow(data)){
-    if (nchar(data$Sample[j]) == 14){
-      data$Sample[j] <- paste(substr(data$Sample[j], 11, 11), "0", substr(data$Sample[j], 12,14), sep = "")
-    }
-    if (nchar(data$Sample[j]) == 15){
-      data$Sample[j] <- substr(data$Sample[j], 11, 15)
-    }
-    if (nchar(data$Sample[j]) == 18){
-      data$Sample[j] <- substr(data$Sample[j], 14, 18)
-    }
-    if (nchar(data$Sample[j]) == 10){
-      data$Sample[j] <- substr(data$Sample[j], 6, 10)
-    }
-    if (nchar(data$Sample[j]) == 17){
-      data$Sample[j] <- substr(data$Sample[j], 13, 17)
-    }
-  }
-  data$Plate <- paste(data$Sample[1], "-", data$Sample[nrow(data)], sep = "")
-  final <- rbind(final, data)
-  Sys.sleep(20)
-}
-# write.csv(final, file = "fullplatelist1.csv", row.names = F)
+# take only the first 8 rows
+sheet <- sheet[1:8, ]
 
+# convert wide data to long data
+data <- reshape2::melt(sheet, id.vars = "Row", variable.name = "Col", value.name = "Sample")
 
-# # fix naming mistakes
-# final$Sample[which(final$Sample == "APCL15_3733511E2579")] <- "E2579"
-# final$Sample[which(final$Sample == "XE1253")] <- "E1253"
-# final$Sample[which(final$Sample == "XE1254")] <- "E1254"
-# final$Sample[which(final$Sample == "20430")] <- "E2430"
-# final$Sample[which(final$Sample == "these columns were combined during extraction")] <- NA
-# final$Sample[which(final$Sample == "BALANCE/neg control")] <- NA
-# final$Plate[which(final$Plate == "E1962-BALANCE/neg control")] <- "E1962-E2043"
+# remove any rows that do not contain samples
+data <- data[!is.na(data$Sample), ]
 
-final <- final[!is.na(final$Sample), ]
-
-# write.csv(final, file = "fullplatelist1.csv", row.names = F)
-# final <- read.csv("fullplatelist1.csv", stringsAsFactors = F)
-# 
-# extracts <- final[1,]
-# extracts[1, ] <- NA
-# 
-# digests <- final[1, ]
-# digests[1, ] <- NA
-# 
-# final_backup <- final
-# 
-# 
-# # split out extracts, digests, ligations and junk
-# for (i in 1:nrow(final)){
-#   if (substr(final$Sample[i], 1, 1) == "E"){
-#     extracts[i, ] <- final[i,]
-#     final[i, ] <- NA
+# # if the plate map contains sample_ids as well as lab_ids (example APCL13_225D0342), remove the sample ID from the lab ID, want only lab ID left (ex: D0342)
+# for (j in 1:nrow(data)){
+#     if (nchar(data$Sample[j]) == 14){
+#       data$Sample[j] <- paste(substr(data$Sample[j], 11, 11), "0", substr(data$Sample[j], 12,14), sep = "")
+#     }
+#     if (nchar(data$Sample[j]) == 15){
+#       data$Sample[j] <- substr(data$Sample[j], 11, 15)
+#     }
+#     if (nchar(data$Sample[j]) == 18){
+#       data$Sample[j] <- substr(data$Sample[j], 14, 18)
+#     }
+#     if (nchar(data$Sample[j]) == 10){
+#       data$Sample[j] <- substr(data$Sample[j], 6, 10)
+#     }
+#     if (nchar(data$Sample[j]) == 17){
+#       data$Sample[j] <- substr(data$Sample[j], 13, 17)
+#     }
 #   }
-# }
-# extracts <- extracts[!is.na(extracts$Sample), ]
-# 
-# for (i in 1:nrow(final)){
-#   if (!is.na(final$Sample[i]) & substr(final$Sample[i], 1, 1) == "D"){
-#     digests[i, ] <- final[i,]
-#     final[i, ] <- NA
-#   }
-# }
-# digests <- digests[!is.na(digests$Sample), ]
-# 
-# for (i in 1:nrow(final)){
-#   if (!is.na(final$Sample[i]) & substr(final$Sample[i], 1, 1) == "L"){
-#     ligs[i, ] <- final[i,]
-#     final[i, ] <- NA
-#   }
-# }
-# ligs <- ligs[!is.na(ligs$Sample), ]
-# 
-# final <- final[!is.na(final$Row), ]
-# 
-# write.csv(extracts, file = "fullextractlist.csv", row.names = F)
-# write.csv(digests, file = "fulldigestlist.csv", row.names = F)
-# write.csv(ligs, file = "fullligationlist.csv", row.names = F)
-# write.csv(final_backup, file = "fullplatelist.csv", row.names = F)
-# 
-# # test <- read.csv("fullextractlist.csv")
-# 
-# # look for errors in  plate names
-# issue <- extracts[which(nchar(extracts$Plate) != 11),]
-# issue <- digests[which(nchar(digests$Plate) != 11),]
+  
+# sort the plate by lab ID and name the plate by the first and last sample
+data <- arrange(data, Sample)
+data$Plate <- paste(data$Sample[1], "-", data$Sample[nrow(data)], sep = "")
 
-# pull in all of the  data
-suppressMessages(library(dplyr))
-labor <- src_mysql(dbname = "Laboratory", host = "amphiprion.deenr.rutgers.edu", user = "michelles", password = "larvae168", port = 3306, create = F)
+# write.csv(data, file = paste("data/", data$Plate[1], "_list.csv", sep = ""), row.names = F)
+
+# pull in all of the  data from the database
+# suppressMessages(library(dplyr))
+labor <- src_mysql(dbname = "Laboratory", default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
 
 suppressWarnings(samples <- labor %>% tbl("digest") %>% collect())
 
 # create backup file of database table
 write.csv(samples, file = paste(Sys.time(), "digestbackup.csv"), row.names = F)
 
-# open the extract list containing wells and plates
-# wells <- read.csv("data/fulldigestlist.csv", stringsAsFactors = F)
-wells <- final
-wells$well <- paste(wells$Row, wells$Col, sep = "")
-wells$Row <- NULL
-wells$Col <- NULL
-# colnames(wells) <- c("digest_id", "plate", "well")
-colnames(wells) <- c("digest_id", "new_plate", "new_well")
+# join the row and column into a well
+data$well <- paste(data$Row, data$Col, sep = "")
+data$Row <- NULL
+data$Col <- NULL
+colnames(data) <- c("digest_id", "new_plate", "new_well")
 
-# try writing out a csv and importing it as an update
-final2 <- left_join(wells, samples, by = "digest_id")
-final2$well <- NULL
-final2$plate <- NULL
-colnames(final2) <- c("digest_id", "plate", "well",  "extraction_id", "date", "vol_in",    "ng_in",     "enzymes",   "final_vol", "quant","DNA_ng",    "notes",     "correction" ,   "corr_message" , "corr_editor", "corr_date" )
+# join the new plate data to the database data
+final <- left_join(samples, data, by = "digest_id")
 
-write.csv(final2, file = "data/digestsfordb.csv", row.names = F)
-
-# join the well data to the database data
-final1 <- left_join(samples, wells, )
-
-for (i in 1:nrow(final1)){
-  if (!is.na(final1$new_well[i])){
-    final1$well[i] <- final1$new_well[i]
-    final1$plate[i] <- final1$plate[i]
+# have to smoosh the new columns into the old columns
+for (i in 1:nrow(final)){
+  if (!is.na(final$new_well[i])){
+    final$well[i] <- final$new_well[i]
+    final$plate[i] <- final$new_plate[i]
   }
 }
+final$new_well <- NULL
+final$new_plate <- NULL
 
 
 # append to the data in the database using RMySQL
 library(RMySQL)
-labors <- dbConnect(MySQL(), host="amphiprion.deenr.rutgers.edu", user="michelles", password="larvae168", dbname="Laboratory", port=3306)
+labors <- dbConnect(MySQL(), dbname="Laboratory", default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL )
+
 
 # Send data to database
 dbWriteTable(labors,"digest",data.frame(final), row.names = FALSE, overwrite = TRUE)
 
 dbDisconnect(labors)
-rm(labors)
+gc(labor)
+rm(labors, labor)
+
