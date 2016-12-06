e6a8a67ec88fff1fd6cd7f593e50b2cccc3c80db
Michelle Stuart <bunx21@yahoo.com>
2016-12-06 17:09:18 -0500

    working

--- R/ligplatefromdb.R
+++ R/ligplatefromdb.R
@@ -8,8 +8,8 @@ suppressMessages(library(dplyr))
 labor <- src_mysql(dbname = "Laboratory", default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
 
 # choose a pool combination to create a map for
-pool1 <- "P020"
-pool2 <- "P021"
+pool1 <- "P016"
+pool2 <- "P017"
 
 suppressWarnings(pool <- labor %>% tbl("ligation") %>% select(pool, barcode_num,digest_id,ligation_id) %>% filter(pool == pool1 | pool == pool2) %>% arrange(pool, barcode_num) %>% collect())
 
@@ -22,11 +22,11 @@ plate1$ligation_id <- as.character(plate1$ligation_id)
 platemap <- as.matrix(reshape2::acast(plate1,plate1[,1] ~ plate1[,2]))
 write.csv(platemap, file = paste("data/", first, "-",last, "map.csv", sep = ""))
 
-# view sources
-source1 <- cbind(plate, pool$digest_id)
-names(source1) <- c("Row", "Col", "digest_id")
-write.csv(source1, file = paste("data/source_", first, "-", last, "list.csv", sep = ""))
-source1$digest_id <- as.character(source1$digest_id)
-platemap <- as.matrix(reshape2::acast(source1,source1[,1] ~ source1[,2]))
-write.csv(platemap, file = paste("data/source_", first, "-",last, "map.csv", sep = ""))
-
+# # view sources
+# source1 <- cbind(plate, pool$digest_id)
+# names(source1) <- c("Row", "Col", "digest_id")
+# write.csv(source1, file = paste("data/source_", first, "-", last, "list.csv", sep = ""))
+# source1$digest_id <- as.character(source1$digest_id)
+# platemap <- as.matrix(reshape2::acast(source1,source1[,1] ~ source1[,2]))
+# write.csv(platemap, file = paste("data/source_", first, "-",last, "map.csv", sep = ""))
+# 
