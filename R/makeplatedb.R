7cf8dadc6fe3a3ef54238425fbacf501ba6e2412
Michelle Stuart <bunx21@yahoo.com>
2017-01-09 15:51:09 -0500

    add variable names

--- R/makeplatedb.R
+++ R/makeplatedb.R
@@ -1,12 +1,21 @@
 # this is a script to make a plate from the locations in the database
 
-mkplatedb <- function(){
-  plate <- data.frame( Row = rep(LETTERS[1:8], 12), Col = unlist(lapply(1:12, rep, 8)))
-  
-  
-  plate1 <- cbind(plate,ID)
-  names(plate1) <- c("Row", "Col", "ID")
-  first <- plate1$ID[1]
-  last <- plate1$ID[nrow(plate1)]
-  return(plate1)
+mkplatedb <- function(x){
+  # pull plate data from db
+  source("R/conlabor.R")
+  labor <- conlabor()
+  if (substr(x, 1,1) == "L"){
+    plate <- labor %>% tbl("ligation") %>% filter(plate == x) %>% select(ligation_id, well) %>% collect()
+  }
+  if (substr(x, 1,1) == "D"){
+    plate <- labor %>% tbl("digest") %>% filter(plate == x) %>% select(digest_id, well) %>% collect()
+  }
+  if (substr(x, 1,1) == "E"){
+    plate <- labor %>% tbl("extraction") %>% filter(plate == x) %>% select(extraction_id, well) %>% collect()
+  }
+  plate$row <- substr(plate$well, 1, 1)
+  plate$col <- substr(plate$well, 2, 3)
+  plate$well <- NULL
+  names(plate) <- c("id, row, col")
+  return(plate)
 }
